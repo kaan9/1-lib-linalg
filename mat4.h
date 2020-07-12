fb0352d0cf7606654c4b23f79c2b745094276592
kaan <kaan803@gmail.com>
2020-07-12 18:37:45 +0300

    impl matrix/vector products

--- mat4.h
+++ mat4.h
@@ -13,10 +13,11 @@ extern struct mat4 mat4_sum(struct mat4 m, struct mat4 n);
 
 extern struct mat4_prod_f(struct mat4 m, float c);
 
+/* 4x4 matrix times a 4x1 vector */
 extern struct mat4 mat4_prod_mat4(struct mat4 m, struct mat4 n);
-
-extern struct mat4 mat4_prod_vec4(struct mat4 m, struct vec4 v);
-
+/* 4x4 matrix times a 4x4 vector */
+extern struct vec4 mat4_prod_vec4(struct mat4 m, struct vec4 v);
+/* 1x4 vector times a 4x4 matrix */
 extern struct vec4 vec4_prod_mat4(struct vec4 v, struct mat4 m);
 
 
@@ -26,8 +27,6 @@ extern struct mat4 mat4_transpose(struct mat4 m);
 
 extern struct vec4 mat4_row(struct mat4 m, unsigned int index);
 
-/* identity matrix */
-extern struct mat4 mat4_make_id();
 /* diagonal matrix */
 extern struct mat4 mat4_make_diag(float c);
 /* Rodrigues's formula for 3d rotation matrix by an angle around an xyz axis */
@@ -45,40 +44,61 @@ extern struct mat4 mat4_make_scale(float x, float y, float z);
 		vec4_sum(m.vs[3], n.vs[3]),	\
 	 })
 
-#define mat4_prod_f(m, c)			\
-	do {					\
-		vec4_prod((m)->vs[0], c);	\
-		vec4_prod((m)->vs[1], c);	\
-		vec4_prod((m)->vs[2], c);	\
-		vec4_prod((m)->vs[3], c);	\
-	} while (0)
+#define mat4_prod_f(m, c)		\
+	((struct mat4) {		\
+		vec4_prod(m.vs[0], c),	\
+		vec4_prod(m.vs[1], c),	\
+		vec4_prod(m.vs[2], c),	\
+		vec4_prod(m.vs[3], c),	\
+	 })
 
 #define mat4_prod_mat4(m, n)			\
-	do {					\
-						\
-	} while (0)
+	((struct mat4) {			\
+		mat4_prod_vec4(m, n.vs[0]),	\
+		mat4_prod_vec4(m, n.vs[1]),	\
+		mat4_prod_vec4(m, n.vs[2]),	\
+		mat4_prod_vec4(m, n.vs[3])	\
+	})
 
 #define mat4_prod_vec4(m, v)			\
-	do {					\
-						\
-	} while (0)
+	((struct vec4) {			\
+		vec4_dot(mat4_row(m, 0), v),	\
+		vec4_dot(mat4_row(m, 1), v),	\
+		vec4_dot(mat4_row(m, 2), v),	\
+		vec4_dot(mat4_row(m, 3), v)	\
+	 })
 
-#define mat4_neg(m)				\
-	do {					\
-		vec4_neg((m)->vs[0])		\
-		vec4_neg((m)->vs[1])		\
-		vec4_neg((m)->vs[2])		\
-		vec4_neg((m)->vs[3])		\
-	} while (0)
+#define vec4_prod_mat4(m, v)		\
+	((struct vec4) {		\
+		vec4_dot(v, m.vs[0])),	\
+		vec4_dot(v, m.vs[1])),	\
+		vec4_dot(v, m.vs[2])),	\
+		vec4_dot(v, m.vs[3]))	\
+	 })
+
+
+#define mat4_neg(m)			\
+	((struct mat4) {		\
+		vec4_neg(m.vs[0]),	\
+		vec4_neg(m.vs[1]),	\
+		vec4_neg(m.vs[2]),	\
+		vec4_neg(m.vs[3]),	\
+	 })
 
 #define mat4_transpose(m)			\
 	
-#define vec4_prod_mat4(v, m)			\
-
 #define mat4_row(m, i)				\
-	((vec4) {				\
+	((struct vec4) {			\
 		((float[]) m->vs[0])[i],	\
 		((float[]) m->vs[1])[i],	\
 		((float[]) m->vs[2])[i],	\
 		((float[]) m->vs[3])[i],	\
 	})
+
+#define mat4_make_diag()				\
+	((struct mat4) {			\
+		((struct vec4) {c, 0, 0, 0}),	\
+	 	((struct vec4) {0, c, 0, 0}),	\
+		((struct vec4) {0, 0, c, 0}),	\
+		((struct vec4) {0, 0, 0, c})	\
+	 })
